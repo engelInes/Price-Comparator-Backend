84ad69e195e8bd530ec3632e307daba42e09c48a
inesEngel <ines.engel@stud.ubbcluj.ro>
2025-05-11 11:36:41 +0300

    created project structure

---
+++ price-comparator/src/main/java/org/example/model/Discount.java
@@ -0,0 +1,87 @@
+package org.example.model;
+
+import java.time.LocalDate;
+
+public class Discount {
+    private String productId;
+    private String productName;
+    private String brand;
+    private double packageQuantity;
+    private String packageUnit;
+    private String productCategory;
+    private LocalDate startingDate;
+    private LocalDate endingDate;
+    private double percentageOfDiscount;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPackageQuantity() {
+        return packageQuantity;
+    }
+
+    public void setPackageQuantity(double packageQuantity) {
+        this.packageQuantity = packageQuantity;
+    }
+
+    public String getPackageUnit() {
+        return packageUnit;
+    }
+
+    public void setPackageUnit(String packageUnit) {
+        this.packageUnit = packageUnit;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public LocalDate getFromDate() {
+        return startingDate;
+    }
+
+    public void setFromDate(LocalDate fromDate) {
+        this.startingDate = fromDate;
+    }
+
+    public LocalDate getEndingDate() {
+        return endingDate;
+    }
+
+    public void setEndingDate(LocalDate endingDate) {
+        this.endingDate = endingDate;
+    }
+
+    public double getPercentageOfDiscount() {
+        return percentageOfDiscount;
+    }
+
+    public void setPercentageOfDiscount(double percentageOfDiscount) {
+        this.percentageOfDiscount = percentageOfDiscount;
+    }
+}
---
+++ price-comparator/src/main/java/org/example/model/Product.java
@@ -0,0 +1,76 @@
+package org.example.model;
+
+public class Product {
+    private String productId;
+    private String productName;
+    private String productCategory;
+    private String brand;
+    private double packageQuantity;
+    private String packageUnit;
+    private double price;
+    private String currency;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPackageQuantity() {
+        return packageQuantity;
+    }
+
+    public void setPackageQuantity(double packageQuantity) {
+        this.packageQuantity = packageQuantity;
+    }
+
+    public String getPackageUnit() {
+        return packageUnit;
+    }
+
+    public void setPackageUnit(String packageUnit) {
+        this.packageUnit = packageUnit;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
